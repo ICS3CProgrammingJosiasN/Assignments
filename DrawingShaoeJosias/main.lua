25425e3a61ee94c31db8e8fba9548bbb974b9b9a
s456389 <55556053+s456389@users.noreply.github.com>
2019-10-04 18:44:40 -0400

    Update main.lua

--- DrawingShaoeJosias/main.lua
+++ DrawingShaoeJosias/main.lua
@@ -1,3 +1,7 @@
+-- Title: Drawing shapes
+-- Name: Josias
+-- Course: ICS2O/3C
+-- This program Displays shapes 
 -- hide status bar 
 display.setStatusBar(display.HiddenStatusBar)
 
@@ -12,10 +16,14 @@ quadilateral.strokeWidth = 20
 
 local parallelogram = display.newPolygon( 768, 256, {80,-20, 0,60, -120,60, -40,-20} )
 
+-- created triangle
 local triangle = display.newPolygon( 450, 256, {0,-80, 80,0, -80,0} )
 
-local shape = display.newPolygon (192, 576, {40,-120, 80,-80, 80,-40, 40,0, -40,0, -80,-40, -80,-80, -40,-120} )
+-- created octagon 
+local octagon = display.newPolygon (192, 576, {40,-120, 80,-80, 80,-40, 40,0, -40,0, -80,-40, -80,-80, -40,-120} )
 
-local shape2 = display.newPolygon(450, 576, {60,-60, 0,80, -60,-60, 0,0} )
+-- created irregular quadilateral
+local irregular quadilateral = display.newPolygon(450, 576, {60,-60, 0,80, -60,-60, 0,0} )
 
+-- created hexagon
 local hexagon = display.newPolygon(768, 576, {40,-120, 80,-60, 40,0 -40,0 -80,-60, -40,-120} )
