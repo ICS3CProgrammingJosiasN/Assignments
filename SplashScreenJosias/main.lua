500da109e160bcaf11819cda7cd0de1e81d35d35
s456389 <55556053+s456389@users.noreply.github.com>
2019-11-11 16:04:38 -0500

    new

--- SplashScreenJosias/main.lua
+++ SplashScreenJosias/main.lua
@@ -1,7 +1,59 @@
 -----------------------------------------------------------------------------------------
 --
--- main.lua
---
------------------------------------------------------------------------------------------
+-- splash_screen.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that
 
--- Your code here
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local companyLogo
+local scrollXSpeed = 5
+local scrollYSpeed = -3
+
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+-- This function moves the company logo across the screen
+local function movecompanyLogo()
+	print(companyLogo.x)
+    companyLogo.x = companyLogo.x + scrollXSpeed
+    companyLogo.y = companyLogo.y + scrollYSpeed 
+
+if ( companyLogo.x == 510 ) then 
+	scrollYSpeed = 0
+	scrollXSpeed = 0
+end
+
+
+
+end
+
+---------------------------------------------------------------------------------------------
+--CREATING IMAGES 
+---------------------------------------------------------------------------------------------
+
+-- Addind company logo to the screen and placing it
+companyLogo = display.newImageRect("Images/CompanyLogoJosias@2x.png", 200, 200)
+companyLogo.x = 0
+companyLogo.y = 600
+
+-- change the size of th ecompany logo 
+companyLogo.xScale = 2
+companyLogo.yScale = 2
+
+-- set the background colour 
+display.setDefault("background", 250, 250, 250)
+
+----------------------------------------------------------------------------------------------
+--EVENT LISTENERS
+-----------------------------------------------------------------------------------------------
+
+Runtime:addEventListener("enterFrame", movecompanyLogo)
+
