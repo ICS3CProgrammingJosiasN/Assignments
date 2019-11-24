a8ae3bcc3741ff2375a9874e584b713b72c82bbc
s456389 <55556053+s456389@users.noreply.github.com>
2019-11-24 15:10:33 -0500

    Update main.lua

--- SplashScreenJosias/main.lua
+++ SplashScreenJosias/main.lua
@@ -11,9 +11,12 @@
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local companyLogo
-local scrollXSpeed = 5
-local scrollYSpeed = -3
+local companyLogo1
+local companyLogo2
+local scrollXSpeed1 = 5
+local scrollYSpeed1 = -3
+local scrollXSpeed2 = 5 
+local scrollYSpeed2 = 3
 
 
 --------------------------------------------------------------------------------------------
@@ -21,14 +24,30 @@ local scrollYSpeed = -3
 --------------------------------------------------------------------------------------------
 
 -- This function moves the company logo across the screen
-local function movecompanyLogo()
-	print(companyLogo.x)
-    companyLogo.x = companyLogo.x + scrollXSpeed
-    companyLogo.y = companyLogo.y + scrollYSpeed 
+local function movecompanyLogo1()
+	print(companyLogo1.x)
+    companyLogo1.x = companyLogo1.x + scrollXSpeed1
+    companyLogo1.y = companyLogo1.y + scrollYSpeed1 
 
-if ( companyLogo.x == 510 ) then 
-	scrollYSpeed = 0
-	scrollXSpeed = 0
+if ( companyLogo1.x == 510 ) then 
+	scrollYSpeed1 = 0
+	scrollXSpeed1 = 0
+end
+
+
+
+end
+
+
+-- This function moves the company logo across the screen
+local function movecompanyLogo2()
+	print(companyLogo2.x)
+    companyLogo2.x = companyLogo2.x + scrollXSpeed2
+    companyLogo2.y = companyLogo2.y + scrollYSpeed2
+
+if ( companyLogo2.x == 510/1 ) then 
+	scrollYSpeed2 = 0
+	scrollXSpeed2 = 0
 end
 
 
@@ -40,13 +59,22 @@ end
 ---------------------------------------------------------------------------------------------
 
 -- Addind company logo to the screen and placing it
-companyLogo = display.newImageRect("Images/CompanyLogoJosias@2x.png", 200, 200)
-companyLogo.x = 0
-companyLogo.y = 600
+companyLogo1 = display.newImageRect("Images/CompanyLogoJosias@2x.png", 200, 200)
+companyLogo1.x = 0
+companyLogo1.y = 600
 
 -- change the size of th ecompany logo 
-companyLogo.xScale = 2
-companyLogo.yScale = 2
+companyLogo1.xScale = 2
+companyLogo1.yScale = 2
+
+-- Addind company logo to the screen and placing it
+companyLogo2 = display.newImageRect("Images/CompanyLogoJosias@2x.png", 200, 200)
+companyLogo2.x = 0
+companyLogo2.y = 0
+
+-- change the size of th ecompany logo 
+companyLogo2.xScale = 2
+companyLogo2.yScale = 2
 
 -- set the background colour 
 display.setDefault("background", 250, 250, 250)
@@ -55,5 +83,6 @@ display.setDefault("background", 250, 250, 250)
 --EVENT LISTENERS
 -----------------------------------------------------------------------------------------------
 
-Runtime:addEventListener("enterFrame", movecompanyLogo)
+Runtime:addEventListener("enterFrame", movecompanyLogo1)
+Runtime:addEventListener("enterFrame", movecompanyLogo2)
 
